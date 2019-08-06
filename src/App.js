f9e4d8fafb3ddc694121e3400c7c5821cf8be101
Sungyeon <sungyeonu@gmail.com>
2019-08-06 14:58:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({ hits: [] })
+  useEffect(async () => {
+    fetch("addLINK")
+    .then(res => res.json())
+    .then(res => this.setState)
+  })
   return (
     <div className="App">
       <header className="App-header">
