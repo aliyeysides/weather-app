35ff63ed7db22a839a38e701a67e4d74ea9ecc6f
Casper Yoo <47223114+sungyeonu@users.noreply.github.com>
2019-08-15 22:41:42 -0400

    added props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Box from '@material-ui/core/Box'
 import './App.css'
-import WeatherCard from './components/WeatherCard';
+import CardList from './components/CardList';
 import Form from './components/Form';
 import Grid from '@material-ui/core/Grid';
 
@@ -12,7 +12,9 @@ function handleSubmit() {
 
 function App() {
   const [data, setData] = useState({ hits: [] });
+  // const [cities, setCities] = useState({city: "Philadelphia", country: "USA"})
 
+  const [cities, setCities] = useState([{city: "Philadelphia", country: "USA"}])
   useEffect(() => {
     const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Philadelphia,US&appid=' + process.env.REACT_APP_WEATHER_API_KEY
     const fetchData = async () => {
@@ -29,7 +31,7 @@ function App() {
   return (
     <Box className="Box">
         <Form onSubmit={handleSubmit}/>
-        <WeatherCard />
+        <CardList cities = {cities}/>
     </Box>
   );
 }
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,13 +3,19 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 
-function WeatherCard() {
+function CardList(props) {
+  //  var t = props.cities.city
+  console.log(props)
+  function createCards(data) {
+    
+  }
   return (
     <Card >
       <p>Card Component</p>
+      {/* <p>{t}</p> */}
       <Button size="small">Learn More</Button>
     </Card>
   )
 }
 
-export default WeatherCard;
+export default CardList;
