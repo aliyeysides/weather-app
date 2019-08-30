f20289b69e8d91985221974afa368aaccbb4e308
Casper Yoo <47223114+sungyeonu@users.noreply.github.com>
2019-08-30 11:47:23 -0400

    Added date

--- src/components/Weather/TimeForm.js
+++ src/components/Weather/TimeForm.js
@@ -5,10 +5,13 @@ function valuetext(value) {
   return `${value} h`;
 }
 
+const today = new Date();
+const date = today.getFullYear() + '-' + (today.getMonth()+1)+'-'+today.getDate();
+
 function TimeForm() {
   return (
     <div className="time-form">
-      <p className="current-time">Current Time - 00:00:00</p>
+      <p className="current-time">Current Time - {date}</p>
       <p>Set Day</p>
       <Slider defaultValue={1} getAreaValueText={valuetext} aria-labelledby="discrete-slider" valueLabelDisplay="auto" step={1} marks min={1} max={5}></Slider>  
       <p>Set Hours</p>
