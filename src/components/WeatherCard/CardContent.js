262fe7c2815e207ea3537d958b10c36da6a62194
aliyeysides <ali.yeysides@gmail.com>
2019-09-07 02:17:13 -0400

    Runs pretty quick

--- src/components/WeatherCard/CardContent.js
+++ src/components/WeatherCard/CardContent.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import { Grid } from '@material-ui/core/';
+import React from "react";
+import { Grid } from "@material-ui/core/";
 
 function CardContent(props) {
   function humidity() {
     if (props.list) {
       console.log(props.list);
     }
-  return (<i className="wi wi-windy"></i>)
+    return <i className="wi wi-windy"></i>;
   }
   return (
-    <Grid container className="forecast" direction="row" justify="flex-start" alignItems="stretch">
+    <Grid
+      container
+      className="forecast"
+      direction="row"
+      justify="flex-start"
+      alignItems="stretch"
+    >
       <Grid item xs={6}>
         <i className="wi wi-day-storm-showers"></i>
       </Grid>
       <Grid item xs={6}>
         <i className="wi wi-meteor"></i>
         <p> {humidity()}</p>
-        <p> Wind: {  } </p>
+        <p> Wind: {} </p>
       </Grid>
     </Grid>
-  )
+  );
 }
 
 export default CardContent;
--- src/components/WeatherCard/CardHeader.js
+++ src/components/WeatherCard/CardHeader.js
@@ -1,40 +1,43 @@
-import React from 'react';
-import { Grid, IconButton   } from '@material-ui/core/';
-import DeleteIcon from '@material-ui/icons/Delete';
+import React from "react";
+import { Grid, IconButton } from "@material-ui/core/";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 function CardHeader(props) {
-  function printTimezone() { 
-    var tz = "UTC"
+  function printTimezone() {
+    var tz = "UTC";
     if (props.timezone === 0) {
-      return tz
+      return tz;
+    } else if (props.timezone > 0) {
+      tz += " +";
+      var t = props.timezone / 60 / 60;
+
+      tz += t;
+    } else {
+      tz += " " + props.timezone / 60 / 60;
     }
-    else if (props.timezone > 0) {
-      tz += " +"
-      var t = (props.timezone/60/60)
-      
-      tz += t
-    }
-    else {
-      tz += " " + (props.timezone/60/60)
-    }
-    return tz
+    return tz;
   }
   return (
-
-    <Grid container className="card-header" direction="row" justify="space-between" alignItems="center">
+    <Grid
+      container
+      className="card-header"
+      direction="row"
+      justify="space-between"
+      alignItems="center"
+    >
       <Grid item xs={7}>
         {props.location.city}, {props.location.country}
       </Grid>
       <Grid item xs={3}>
-        { printTimezone() }
+        {printTimezone()}
       </Grid>
       <Grid item xs={2}>
-        <IconButton onClick={props.deleteIcon}aria-label="delete">
+        <IconButton onClick={props.deleteIcon} aria-label="delete">
           <DeleteIcon />
-        </IconButton>   
+        </IconButton>
       </Grid>
     </Grid>
-  )
+  );
 }
 
 export default CardHeader;
